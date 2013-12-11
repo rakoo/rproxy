48816cea0fd643bd8dd13be4912333e7f10c46d4
Matthieu Rakotojaona <matthieu.rakotojaona@gmail.com>
2013-12-11 21:02:31 +0100

    Working version
    Add all the necessary code for a running rproxy pair, using host binary

--- client/main.go
+++ client/main.go
@@ -1,23 +1,151 @@
 package main
 
 import (
+	"bytes"
+	_ "compress/gzip"
+	"crypto/sha1"
+	"encoding/base64"
+	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/elazarl/goproxy"
 )
 
 func main() {
-	url, err := url.Parse("http://localhost:8080")
+	proxy := goproxy.NewProxyHttpServer()
+
+	rp := NewRProxy()
+	proxy.OnRequest().Do(goproxy.FuncReqHandler(rp.addSignature))
+	proxy.OnResponse().Do(goproxy.FuncRespHandler(rp.patch))
+
+	http.Handle("/", proxy)
+	log.Println("Starting listening on :2424")
+	log.Fatal(http.ListenAndServe(":2424", nil))
+}
+
+type RProxy struct {
+	cacheFile *os.File
+}
+
+func NewRProxy() *RProxy {
+	f, err := os.Create("file")
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Couldn't open file:", err)
+	}
+	return &RProxy{f}
+}
+
+func (rp *RProxy) HasCache() bool {
+	return rp.cacheFile != nil && len(rp.ReadCache()) != 0
+}
+func (rp *RProxy) ReadCache() (content []byte) {
+	_, err := rp.cacheFile.Seek(0, os.SEEK_SET)
+	if err != nil {
+		log.Println("Couldn't seek to beginning:", err)
+		return
 	}
 
-	rp := httputil.NewSingleHostReverseProxy(url)
-
-  log.Println("Starting server proxy on :2424")
-  err = http.ListenAndServe(":2424", rp)
-  if err != nil {
-    log.Fatal(err)
-  }
+	var b bytes.Buffer
+	_, err = io.Copy(&b, rp.cacheFile)
+	if err != nil {
+		log.Println("Couldn't copy file to buffer:", err)
+		return
+	}
+	return b.Bytes()
 }
+func (rp *RProxy) WriteCache(p []byte) {
+	_, err := rp.cacheFile.Seek(0, os.SEEK_SET)
+	if err != nil {
+		log.Println("Couldn't seek to beginning:", err)
+		return
+	}
+
+	rp.cacheFile.Write(p)
+	rp.cacheFile.Sync()
+}
+
+func (rp *RProxy) addSignature(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	r.URL.Host = "localhost:8080"
+	r.Header.Set("X-GoProxy", "yxorPoG-X")
+
+	if !rp.HasCache() {
+		return r, nil
+	}
+
+	sha := sha1.New()
+	sha.Write(rp.ReadCache())
+	log.Printf("Signing %x", sha.Sum(nil))
+
+	var b bytes.Buffer
+	enc := base64.NewEncoder(base64.URLEncoding, &b)
+
+	rdiff := exec.Command("/usr/bin/rdiff", "signature", "-")
+	rdiff.Stdin = bytes.NewReader(rp.ReadCache())
+	rdiff.Stdout = enc
+
+	err := rdiff.Run()
+	if err != nil {
+		log.Println("Error running rdiff signature:", err)
+		return r, nil
+	}
+
+	// TODO: maybe wrap enc in a gzip.Writer. Has shown to actually take
+	// more place, since signature should be small (<1024B)
+	// If enc is a gzip.Writer, don't forget to flush it:
+	// enc.Flush()
+
+	log.Printf("Sending %d bytes as X-RProxy-Sig", b.Len())
+
+	r.Header.Set("X-RProxy-Sig", string(b.Bytes()))
+	return r, nil
+}
+
+func (rp *RProxy) patch(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	ct := r.Header.Get("Content-Type")
+	required := "application/rproxy-patch"
+
+	if !rp.HasCache() || ct != required && !strings.HasPrefix(ct, required+";") {
+		var b bytes.Buffer
+		io.Copy(&b, r.Body)
+		rp.WriteCache(b.Bytes())
+
+		r.Body = &bytesCloser{b}
+		return r
+	}
+
+	defer r.Body.Close()
+
+	var body bytes.Buffer
+	_, err := io.Copy(&body, r.Body)
+	if err != nil {
+		log.Println("Couldn't copy body: ", err)
+		return r
+	}
+
+	var patched bytes.Buffer
+
+	rdiff := exec.Command("/usr/bin/rdiff", "patch", "file", "-", "-")
+	rdiff.Stdin = &body
+	rdiff.Stdout = &patched
+
+	err = rdiff.Run()
+	if err != nil {
+		log.Println("Error running rdiff patch:", err)
+	}
+
+	rp.WriteCache(patched.Bytes())
+	r.Body = &bytesCloser{patched}
+
+	//log.Printf("Patch is %d bytes long; full content is %d bytes long", body.Len(), patched.Len())
+	return r
+}
+
+type bytesCloser struct {
+	bytes.Buffer
+}
+
+func (bc *bytesCloser) Close() error { return nil }
--- server/main.go
+++ server/main.go
@@ -1,23 +1,142 @@
 package main
 
 import (
+	"bytes"
+	"encoding/base64"
+	"io"
 	"log"
 	"net/http"
-	"net/http/httputil"
-	"net/url"
+	"os"
+	"os/exec"
+	"strings"
+
+	"github.com/elazarl/goproxy"
 )
 
 func main() {
-	url, err := url.Parse("http://localhost:9807")
+	proxy := goproxy.NewProxyHttpServer()
+
+	rp := NewRproxy()
+	proxy.OnRequest().Do(goproxy.FuncReqHandler(rp.storeSig))
+	proxy.OnResponse().Do(goproxy.FuncRespHandler(rp.delta))
+
+	http.Handle("/", proxy)
+	log.Println("Starting listening on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+type RProxy struct {
+	sig *os.File
+}
+
+func NewRproxy() *RProxy {
+	sigFile, err := os.Create("sig")
 	if err != nil {
-		log.Fatal(err)
+		log.Println("Couldn't create sig:", err)
 	}
 
-	rp := httputil.NewSingleHostReverseProxy(url)
-
-  log.Println("Starting server proxy on :8080")
-  err = http.ListenAndServe(":8080", rp)
-  if err != nil {
-    log.Fatal(err)
-  }
+	return &RProxy{sigFile}
 }
+
+func (rp *RProxy) HasSig() bool {
+	return rp.sig != nil && len(rp.ReadSig()) != 0
+}
+
+func (rp *RProxy) ReadSig() (c []byte) {
+	if rp.sig == nil {
+		return
+	}
+
+	_, err := rp.sig.Seek(0, os.SEEK_SET)
+	if err != nil {
+		log.Println("Couldn't seek to beginning:", err)
+		return
+	}
+
+	var b bytes.Buffer
+	_, err = io.Copy(&b, rp.sig)
+	if err != nil {
+		log.Println("Couldn't copy to temp buff:", err)
+		return
+	}
+	return b.Bytes()
+}
+
+func (rp *RProxy) WriteSig(p []byte) {
+	if rp.sig == nil {
+		return
+	}
+
+	_, err := rp.sig.Seek(0, os.SEEK_SET)
+	if err != nil {
+		log.Println("Couldn't seek to beginning:", err)
+		return
+	}
+
+	rp.sig.Write(p)
+	rp.sig.Sync()
+}
+
+func (rp *RProxy) storeSig(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
+	r.URL.Host = "localhost:9807"
+	sig64 := r.Header.Get("X-RProxy-Sig")
+
+	if sig64 == "" {
+		return r, nil
+	}
+
+	log.Printf("C -> S: %dB", len(sig64))
+
+	dec := base64.NewDecoder(base64.URLEncoding, strings.NewReader(sig64))
+	var sig bytes.Buffer
+	_, err := io.Copy(&sig, dec)
+	if err != nil {
+		log.Println("Couldn't decode signature:", err)
+		return r, nil
+	}
+	rp.WriteSig(sig.Bytes())
+
+	return r, nil
+}
+
+func (rp *RProxy) delta(r *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
+	if !rp.HasSig() {
+		return r
+	}
+
+	r.Header.Set("Content-Type", "application/rproxy-patch")
+
+	defer r.Body.Close()
+
+	var body bytes.Buffer
+	_, err := io.Copy(&body, r.Body)
+	if err != nil {
+		log.Println("Error recopying body: ", err)
+		return r
+	}
+
+	var delta bytes.Buffer
+	rdiff := exec.Command("/usr/bin/rdiff", "delta", "sig", "-", "-")
+	rdiff.Stdin = &body
+	rdiff.Stdout = &delta
+
+	var errBuf bytes.Buffer
+	rdiff.Stderr = &errBuf
+
+	err = rdiff.Run()
+	if err != nil {
+		log.Println("Error running rdiff delta:", err)
+		log.Println("rdiff error: ", string(errBuf.Bytes()))
+	}
+
+	r.Body = &bytesCloser{delta}
+
+	log.Printf("S -> C: %dB", delta.Len())
+	return r
+}
+
+type bytesCloser struct {
+	bytes.Buffer
+}
+
+func (bc *bytesCloser) Close() error { return nil }
